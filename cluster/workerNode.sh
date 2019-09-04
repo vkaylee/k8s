7c5cf4f63093970728363c9bc464e032266b0e1c
Lee Tuan <lee@vlee.dev>
2019-09-04 22:15:14 +0700

    update

--- cluster/workerNode.sh
+++ cluster/workerNode.sh
@@ -8,5 +8,3 @@ source "${workDir}/criO.sh"
 source "${workDir}/containerd.sh"
 # shellcheck disable=SC1090
 source "${workDir}/kubeTools.sh"
-kubeadm config images pull
-kubeadm init --pod-network-cidr 10.0.0.0/16
