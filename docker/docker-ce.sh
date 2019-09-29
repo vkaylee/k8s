50bef1b6a08102f08de49775f45db73259a34427
Lee Tuan <lee@vlee.dev>
2019-09-29 17:12:08 +0700

    Update the way to install docker

--- docker/docker-ce.sh
+++ docker/docker-ce.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get purge -y docker-ce
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker "$USER"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
---
+++ docker/docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sh ./docker-ce.sh
+sh ./docker-compose.sh
